49efc5eb98bc5b047b835eac98ae15bffdf5a76b
vylia99 <110261644+vylia99@users.noreply.github.com>
2025-05-29 13:08:42 +0200

    Delete part2/connect.py

--- part2/connect.py
+++
@@ -1,7 +0,0 @@
-from mongoengine import connect
-
-connect(
-    db="your_db_name",
-    host="mongodb+srv://yvasilishina:BeHaPpY@cluster0.yszwvuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    alias="default"
-)
